d93e50f8a8a4c31e4a8cd107b74305d9f589b713
maximeKervran <157500888+maximeKervran@users.noreply.github.com>
2025-04-22 16:53:58 +0100

    File updated at 22/04/2025 16:53:58

--- 2-AddTwoNumbers/2-AddTwoNumbers.cpp
+++ 2-AddTwoNumbers/2-AddTwoNumbers.cpp
@@ -1,39 +1,26 @@
-// Last updated: 22/04/2025 16:50:38
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
+// Last updated: 22/04/2025 16:53:58
 class Solution {
 public:
-    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode* dummyNode = new ListNode(0);
-        ListNode* final = dummyNode;
-        int res;
-        int retenue = 0;
-        while(l1 != nullptr || l2 != nullptr || retenue != 0){
-            res = retenue;
-            retenue = 0;
-            if(l1 != nullptr){
-                res = res + l1->val;
-                l1 = l1->next;
-            }
-            if(l2 != nullptr){
-                res = res + l2->val;
-                l2 = l2->next;
-            }
-            if(res >= 10){
-                retenue = res/10;
-                res = res % 10;
-            }
-            final->next = new ListNode(res);
-            final = final->next;
+    bool isPalindrome(int x){
+        int moitie = 0;
+        int i =0;
+
+        if(x<0 || x%10 == 0 && x != 0){
+            return false;
         }
-        return dummyNode->next;
+
+        while(x > moitie){
+            moitie = moitie*10 + x%10;
+            x/= 10;
+            i++;
+        }
+
+        if(x == moitie){
+             return true;
+        }
+        if(x == moitie/10){
+            return true;
+        }
+        return false;
     }
 };
